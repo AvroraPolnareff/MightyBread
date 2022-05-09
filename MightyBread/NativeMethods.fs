d9c07808fd878bb18079d62065605de1743ea520
Avrora Polnareff <AraInTheSkyWith@gmail.com>
2022-05-09 20:43:40 +0300

    add native methods

--- MightyBread/NativeMethods.fs
+++ MightyBread/NativeMethods.fs
@@ -9,25 +9,44 @@ module User32 =
     type EnumWindowsFunc = delegate of IntPtr * int -> bool
 
     [<DllImport("user32.dll")>]
-    extern IntPtr GetWindowDC(IntPtr)
+    extern IntPtr GetWindowDC(IntPtr windowPointer)
     
     [<DllImport("user32.dll")>]
-    extern IntPtr ReleaseDC(IntPtr, IntPtr)
+    extern IntPtr ReleaseDC(IntPtr windowPointer, IntPtr hDC)
 
     [<DllImport("user32.dll")>]
-    extern IntPtr GetWindowRect(IntPtr, Rectangle)
+    extern IntPtr GetWindowRect(IntPtr windowPointer, Rectangle rect)
 
     [<DllImport("user32.dll")>]
-    extern bool EnumWindows(EnumWindowsFunc, int)
+    extern bool EnumWindows(EnumWindowsFunc, int lParam)
     
     [<DllImport("user32.dll")>]
-    extern int GetWindowText(IntPtr, StringBuilder, int)
+    extern int GetWindowText(IntPtr windowPointer, StringBuilder, int lParam)
     
     [<DllImport("user32.dll")>]
-    extern int GetWindowTextLength(IntPtr)
+    extern int GetWindowTextLength(IntPtr windowPointer)
     
     [<DllImport("user32.dll")>]
-    extern bool IsWindowVisible(IntPtr)
+    extern bool IsWindowVisible(IntPtr windowPointer)
     
     [<DllImport("user32.dll")>]
     extern IntPtr GetShellWindow()
+    
+module GDI32 =
+    [<DllImport("gdi32.dll")>]
+    extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hObjectSource, int nXSrc, int nYSrc, int dwRop)
+    
+    [<DllImport("gdi32.dll")>]
+    extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth, int nHeight)
+    
+    [<DllImport("gdi32.dll")>]
+    extern IntPtr CreateCompatibleDC(IntPtr hDC)
+    
+    [<DllImport("gdi32.dll")>]
+    extern IntPtr DeleteDC(IntPtr hDC)
+    
+    [<DllImport("gdi32.dll")>]
+    extern IntPtr DeleteObject(IntPtr hObject)
+    
+    [<DllImport("gdi32.dll")>]
+    extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject)
