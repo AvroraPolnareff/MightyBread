e0aadffa33062fdfb2cd56b08128e0c1fea9f55d
Avrora Polnareff <AraInTheSkyWith@gmail.com>
2022-05-09 18:57:18 +0300

    get rid of some mess

--- MightyBread/Program.fs
+++ MightyBread/Program.fs
@@ -1,25 +1,4 @@
-﻿open System
-open System.Runtime.InteropServices
-open System.Drawing.Imaging
-open System.Drawing
-open System.Text
-open Tesseract
-
-
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
-
-
-
-//let fullScreenshot (filepath: string) (filename: string) (format ImageFormat) =
-//    let bounds =
-
-//type EnumWindowsProc = delegate of (IntPtr * int) -> bool
-
-
-
-
-
+﻿open ScreenCapture
 
 let windowStrings =
     getOpenWindows()
@@ -28,4 +7,3 @@ let windowStrings =
 
 for title in windowStrings do
     printfn $"{title}"
-
